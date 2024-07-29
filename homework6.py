c74e8067fdef6bb5beab747aa2cc9db378e701e3
pati-patimat <patya.khaib@yandex.ru>
2024-07-29 23:57:05 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Nana':2020, 'Masha': 2012, 'Kesha': 2010}
+print(my_dict)
+print(my_dict ['Kesha'])
+print(my_dict.get('Hanna'))
+my_dict.update({'Nika': 2003, 'Kate': 2005})
+print(my_dict)
+my_dict.pop('Masha')
+print(my_dict)
+my_set = {1, 2, 3, 4, 5, 4, 3, 2, 1, 'n', 'k', 'n', 'k', 'g', 'h', (7, 8, 9)}
+print(my_set)
+my_set.update({6, 0, 8})
+print(my_set)
+my_set.discard(3)
+print(my_set)
